e39f8e82812fe914648b6c50cc14ecc4089c1675
EstebanXP <47235718+EstebanXP@users.noreply.github.com>
2019-02-23 18:09:17 -0700

    Add files via upload

--- Clase 6 Algoritmos/Analisis algoritmo de otro equipo/main.cpp
+++ Clase 6 Algoritmos/Analisis algoritmo de otro equipo/main.cpp
@@ -4,33 +4,35 @@ using namespace std;
 
 int main()
 {
-    int arr[]={10,9,9,7,7,6,4,2};
+    int arr[]={10,9,9,7,7,6,4,2,4,1,6,3};
     int i=0, SHORT_RES=0,j=0,n=sizeof(arr)/sizeof(arr[0]);
     cout<<"Arreglo a ordenar: ";
     for(i=0;i<n;i++){ //n veces
-        cout<<arr[i]<<",";
+        cout<<arr[i]<<","; //n veces
     }
     cout<<endl;//1 vez
     for(i=1;i<n;i++){ //n veces
         SHORT_RES=arr[i];
-        j=i-1;//n-1 veces
+        j=i-1;//n veces
         while(j>=0&&arr[j]>SHORT_RES){ //variable aleatoria veces
-            arr[j+1]=arr[j];
+            arr[j+1]=arr[j];//variable aleatoria veces-1
             j=j-1; //variable aleatoria-1 veces
+
         }
-    for(i=1;i<n;i++){ //n-1 veces
-        SHORT_RES=arr[i];
-        j=i-1; //n-2 veces
+    for(i=1;i<n;i++){ //n veces
+        SHORT_RES=arr[i]; //n veces
+        j=i-1; //n-1 veces
         while(j>=0&&arr[j]>SHORT_RES){ //variable aleatoria veces
-            arr[j+1]=arr[j];
-            j=j-1;//variable aleatoria-1
+            arr[j+1]=arr[j]; //variable aleatria-1 veces
+            j=j-1;//variable aleatoria-1 veces
         }
-        arr[j+1]=SHORT_RES;//n-2
+        arr[j+1]=SHORT_RES;//n veces
     }
     }
     cout<<"Arreglo ordenado: ";
     for(i=0;i<n;i++){//n veces
         cout<<arr[i]<<",";
     }
+
     return 0;
 }
